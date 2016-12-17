cc96a2fefdb3c81fb92849cf2d39705bb4f055a3
abhilash.thaduka@gmail.com <athaduka@gainsight.com>
2016-12-17 23:43:48 +0530

    Ready to deploy

--- src/main/java/org/arpit/java2blog/bean/Country.java
+++ src/main/java/org/arpit/java2blog/bean/Country.java
@@ -3,7 +3,7 @@ package org.arpit.java2blog.bean;
 /**
  * Created by Abhilash Thaduka on 12/12/2016.
  */
-public class Country{
+public class Country {
 
     int id;
     String countryName;
@@ -12,16 +12,21 @@ public class Country{
         super();
         this.id = i;
         this.countryName = countryName;
+
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getCountryName() {
         return countryName;
     }
+
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
